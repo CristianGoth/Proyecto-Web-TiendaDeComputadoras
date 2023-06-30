871305ed5874193ac01c6bb4c6ace231d9c4defd
Cristian <101565396+CristianGoth@users.noreply.github.com>
2023-06-30 14:29:02 -0400

    Add files via upload

--- computadores/tienda/urls.py
+++ computadores/tienda/urls.py
@@ -3,19 +3,25 @@ from . import views
 
 urlpatterns = [
     #PAGINA PRINCIPAL
-    path('',views.index_view,name=''),
-    path('index/',views.index_view,name='index'),
-    path('carrito/',views.carrito_view,name='carrito'),
-    path('ofertas/',views.ofertas_view,name='ofertas'),
-    path('productos/',views.productos_view,name='productos'),
-    path('registrosesi/',views.registrosesi_view,name='registrosesi'),
-    path('contacto/',views.contacto_view,name='contacto'),
-    path('inisesion/',views.inisesion_view,name='inisesion'),
+    path('', views.index_view, name='index'),
+    path('index/', views.index_view, name='index'),
+    path('carrito/', views.carrito_view, name='carrito'),
+    path('ofertas/', views.ofertas_view, name='ofertas'),
+    path('productos/', views.productos_view, name='productos'),
+    path('contacto/', views.contacto_view, name='contacto'),
+    path('inisesion/', views.inisesion_view, name='inisesion'),
+    path('inisesion/registrosesi.html', views.registrosesi_view, name='registrosesi_html'),
 
     
 
     #AGREGADO Y ELIMINADO DE PRODUCTO
     path("producto_mod/", views.producto_mod, name="producto_mod"),
+    path('producto_find/<int:pk>/', views.producto_find, name='producto_find'),
     path('producto_add/',views.producto_add, name='producto_add'),
-    path("producto_update/", views.producto_update, name="producto_update"),
+    path('producto_del/<int:pk>/', views.producto_del, name='producto_del'),
+    path('producto_update/<int:pk>/', views.producto_update, name='producto_update'),
+
+
+    # Ruta de página no encontrada (404)
+    path('index/', views.not_found_view, name='not_found'),
 ]
--- computadores/tienda/views.py
+++ computadores/tienda/views.py
@@ -54,36 +54,46 @@ def registrosesi_view(request):
 
 @staff_member_required
 def producto_add(request):
-    
-    if request.method != "POST":
-        lista_categorias = Categoria.objects.all()
-        context = {"categorias":lista_categorias}
-        return render(request,'venta/producto_add.html', context)
-    else:
-        
-        categoria   = request.POST["categoria"]
-        nombre      = request.POST["nombre"]
-        marca       = request.POST["marca"]
+    if request.method == "POST":
+        categoria = request.POST["categoria"]
+        nombre = request.POST["nombre"]
+        marca = request.POST["marca"]
         descripcion = request.POST["detalle"]
-        precio      = request.POST["precio"]
-        stock       = request.POST["stock"]
-        imagen      = request.POST["imagen"]
+        precio = request.POST["precio"]
+        stock = request.POST["stock"]
+        imagen = request.POST["imagen"]
 
-        objCategoria= Categoria.objects.get(id_categoria = categoria)
-        
+        # Validar el campo 'precio'
+        if not precio.isnumeric():
+            lista_categorias = Categoria.objects.all()
+            context = {
+                "categorias": lista_categorias,
+                "mensaje": "El campo 'precio' debe ser un número válido."
+            }
+            return render(request, 'tienda/producto_add.html', context)
+
+        objCategoria = Categoria.objects.get(id_categoria=categoria)
+
         objProducto = Producto.objects.create(
-            id_categoria     = objCategoria,
-            nombre           = nombre,
-            marca            = marca,
-            descripcion      = descripcion,
-            precio           = precio,
-            stock            = stock,
-            imagen           = imagen)
-        
+            id_categoria=objCategoria,
+            nombre=nombre,
+            marca=marca,
+            descripcion=descripcion,
+            precio=precio,
+            stock=stock,
+            imagen=imagen
+        )
+
         objProducto.save()
         lista_categorias = Categoria.objects.all()
-        context = {"mensaje":"Se guardó el producto"}
-        return render(request,'venta/producto_add.html', context)
+        context = {"mensaje": "Se guardó el producto correctamente."}
+        return render(request, 'tienda/producto_add.html', context)
+
+    else:
+        lista_categorias = Categoria.objects.all()
+        context = {"categorias": lista_categorias}
+        return render(request, 'tienda/producto_add.html', context)
+
 
 @staff_member_required
 def producto_mod(request):
@@ -99,12 +109,12 @@ def producto_del(request,pk):
         mensaje = "El producto se ha eliminado"
         lista_productos = Producto.objects.all()
         context = {"productos":lista_productos, "mensaje":mensaje}
-        return render(request,'venta/producto_mod.html', context)
+        return render(request,'tienda/producto_mod.html', context)
     except:
         mensaje = "El producto NO ha sido eliminado"
         lista_productos = Producto.objects.all()
         context = {"productos":lista_productos, "mensaje":mensaje}
-        return render(request,'venta/producto_mod.html', context)
+        return render(request,'tienda/producto_mod.html', context)
 
 @staff_member_required
 def producto_find(request,pk):
@@ -113,50 +123,55 @@ def producto_find(request,pk):
         lista_categorias = Categoria.objects.all() 
         
         context = {"producto":producto, "categorias":lista_categorias}
-        return render(request,'venta/producto_edit.html', context)
+        return render(request,'tienda/productos_edit.html', context)
     else:
         mensaje = "El producto NO existe"
         context = {"mensaje":mensaje}
-        return render(request,'venta/producto_mod.html', context)
+        return render(request,'tienda/producto_mod.html', context)
 
 @staff_member_required
-def producto_update(request):
-    
+def producto_update(request, pk):
+    try:
+        producto = Producto.objects.get(id_producto=pk)
+    except Producto.DoesNotExist:
+        mensaje = "El producto no existe"
+        context = {"mensaje": mensaje}
+        return render(request, 'tienda/producto_mod.html', context)
+
     if request.method == "POST":
-        
-        idprod      = request.POST["idprod"]
-        categoria   = request.POST["categoria"]
-        nombre      = request.POST["nombre"]
-        marca       = request.POST["marca"]
+        categoria = request.POST["categoria"]
+        nombre = request.POST["nombre"]
+        marca = request.POST["marca"]
         descripcion = request.POST["detalle"]
-        precio      = request.POST["precio"]
-        stock       = request.POST["stock"]
+        precio = request.POST["precio"]
+        stock = request.POST["stock"]
 
-        objCategoria = Categoria.objects.get(id_categoria = categoria)
-        objProducto = Producto()
-        
-        
-        objProducto.id_producto     = idprod
-        objProducto.id_categoria    = objCategoria
-        objProducto.nombre          = nombre
-        objProducto.marca           = marca
-        objProducto.descripcion     = descripcion
-        objProducto.precio          = precio
-        objProducto.stock           = stock
-        
-        objProducto.save()
+        objCategoria = Categoria.objects.get(id_categoria=categoria)
+
+        producto.id_categoria = objCategoria
+        producto.nombre = nombre
+        producto.marca = marca
+        producto.descripcion = descripcion
+        producto.precio = precio
+        producto.stock = stock
+
+        producto.save()
         lista_categorias = Categoria.objects.all()
-        context = {"mensaje":"El producto se ha actualizado correctamente", "categorias":lista_categorias, "producto":objProducto}
-        return render(request,'venta/producto_edit.html', context)
+        context = {"mensaje": "El producto se ha actualizado correctamente", "categorias": lista_categorias, "producto": producto}
+        return render(request, 'tienda/productos_edit.html', context)
     else:
-        lista_productos = Producto.objects.all()
-        context = {"productos":lista_productos}
-        return render(request,'venta/producto_mod.html', context)
+        lista_categorias = Categoria.objects.all()
+        context = {"producto": producto, "categorias": lista_categorias}
+        return render(request, 'tienda/productos_edit.html', context)
 
+    
 @login_required
 def usuario(request):
     request.session["usuario"]="lucasfernandezm"
     usuario = request.session["usuario"] 
     context = {"usuario":usuario}
     return render(request,'venta/usuario.html',context)
+
+def not_found_view(request):
+    return render(request, 'tienda/index.html')
 
